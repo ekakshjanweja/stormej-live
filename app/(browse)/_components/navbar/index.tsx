e41c9276b96311fa94543b0d9f9cd65cae7be216
ekaksh <jekaksh@gmail.com>
2024-02-10 02:12:56 +0530

    Sidebar Created

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -5,11 +5,11 @@ import { Search } from "./search";
 export const Navbar = () => {
   return (
     <>
-      <nav className="fixed top-0 w-full h-20 z-[49] bg-[#2C2E36] px-2 lg:px-4 flex justify-between items-center shadow-sm">
+      <div className="fixed top-0 w-full h-20 z-[49] bg-[#2C2E36] px-2 lg:px-4 flex justify-between items-center shadow-sm">
         <Logo />
         <Search />
         <Actions />
-      </nav>
+      </div>
     </>
   );
 };
