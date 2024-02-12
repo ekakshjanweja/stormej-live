969444057c3beee60d5c8bbfccaebcf3e1d4d42d
ekaksh <jekaksh@gmail.com>
2024-02-13 00:55:40 +0530

    Created LiveKit Webhook

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useSidebar } from "@/store/use-sidebar";
-import { Follow, User } from "@prisma/client";
+import { Follow, User, Stream } from "@prisma/client";
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface FollowingProps {
-  data: (Follow & { following: User })[];
+  data: (Follow & { following: User & { stream: Stream | null } })[];
 }
 
 export const Following = async ({ data }: FollowingProps) => {
@@ -29,6 +29,7 @@ export const Following = async ({ data }: FollowingProps) => {
               key={follow.following.id}
               username={follow.following.username}
               imageUrl={follow.following.imageUrl}
+              isLive={follow.following.stream?.isLive}
             />
           ))}
         </ul>
